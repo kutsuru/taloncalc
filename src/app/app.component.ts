a9f27faa1c751d08104ec8ea6ead5889d18ec233
Rchitects <johnnyfikcock@gmail.com>
2022-08-30 15:17:01 +0200

    fix: battle-calc-pvm stylesheet name fixed to scss fix: use logo image instead of encrypted image data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { appLogoData } from './app-logo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  appLogoData = appLogoData;
-}
+export class AppComponent { }
